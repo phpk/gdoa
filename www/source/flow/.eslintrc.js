5237e8f7785680c160952318169b8241fcb442e1
icanwallet <ruitaoliu@qq.com>
2021-11-30 19:29:03 +0800

    add flow

---
+++ www/source/flow/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/essential', '@vue/prettier'],
+    rules: {
+        'prettier/prettier': [
+            'warn',
+            {
+                trailingComma: 'es5',
+                tabWidth: 2,
+                semi: false,
+                singleQuote: true,
+            },
+        ],
+        'no-console': 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-unused-vars': 'warn',
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+}
