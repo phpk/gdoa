0d5a773e6ae171fc616cf19f513587aa1879f7ac
icanwallet <ruitaoliu@qq.com>
2021-12-27 13:06:30 +0800

    remove epub

--- www/admin/epub/js/Construct/LocalFileSystem.js
+++
@@ -1,8 +0,0 @@
-define(function() {
-    var LS = function() {
-        this.clear = function() {
-            localStorage.clear();
-        };
-    };
-    return LS;
-})
--- www/admin/epub/js/config.js
+++
@@ -1,4 +0,0 @@
-var EBConfig = {
-    "lang" : "en"
-    //lang : "zh-cn"
-};
